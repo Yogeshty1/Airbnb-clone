9ac8e2e9b38ccbb2518c8b7da6cd878f2cd5d5d3
Yogeshty1 <your-email@example.com>
2025-09-24 18:41:06 +0530

    Ignore node_modules and remove it from tracking

--- app.js
+++ app.js
@@ -4,14 +4,9 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
-<<<<<<< HEAD
 const ejsMate=require("ejs-mate")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/Wanderlust";
-=======
-
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
->>>>>>> 59e553cfc8d0195a1ae85d49435cad6265e2f87a
 
 main()
   .then(() => {
@@ -29,20 +24,13 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-<<<<<<< HEAD
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
-=======
->>>>>>> 59e553cfc8d0195a1ae85d49435cad6265e2f87a
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 59e553cfc8d0195a1ae85d49435cad6265e2f87a
 //Index Route
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
